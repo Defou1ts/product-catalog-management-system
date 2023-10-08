86e4b9496ff386867f57566813cc0354eb484a04
Defou1ts <pasnkevikn@gmail.com>
2023-10-08 21:24:07 +0300

    feat: added new deps

--- apps/products/src/app.module.ts
+++ apps/products/src/app.module.ts
@@ -1,8 +1,11 @@
 import { postgresConfigRegister } from '@config/config';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { validate } from './config/config.validation';
+
+import type { PostgresConfig } from '@config/config';
 
 @Module({
 	imports: [
@@ -10,6 +13,21 @@ import { validate } from './config/config.validation';
 			isGlobal: true,
 			load: [postgresConfigRegister],
 			validate,
+		}),
+		TypeOrmModule.forRootAsync({
+			useFactory: ({ host, port, username, password, database }: PostgresConfig) => ({
+				type: 'postgres',
+				host,
+				port,
+				username,
+				password,
+				database,
+				entities: [],
+				synchronize: true,
+				autoLoadEntities: true,
+				logging: true,
+			}),
+			inject: [postgresConfigRegister.KEY],
 		}),
 	],
 	controllers: [],
