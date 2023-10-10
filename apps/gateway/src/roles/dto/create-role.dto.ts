454b6ecf5dca2b965f0f5e40140a0b21bed5783b
Defou1ts <pasnkevikn@gmail.com>
2023-10-10 21:27:20 +0300

    fix: deps

--- apps/gateway/src/roles/dto/create-role.dto.ts
+++ apps/gateway/src/roles/dto/create-role.dto.ts
@@ -1,12 +1,6 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateRoleDto {
-	@ApiProperty({ example: 'ORGANIZER', description: 'Unique value of the role' })
 	@IsString()
 	readonly value: string;
-
-	@ApiProperty({ example: 'Organizer of meetups', description: 'Role description' })
-	@IsString()
-	readonly description: string;
 }
