95ac1dff42fd17a113de89abf61ba320757abf3b
Defou1ts <pasnkevikn@gmail.com>
2023-10-10 12:02:01 +0300

    fix: carts service names

--- apps/carts/src/carts/carts.module.ts
+++ apps/carts/src/carts/carts.module.ts
@@ -5,12 +5,11 @@ import { CartsRepository } from './carts.repository';
 import { CartsResolver } from './carts.resolver';
 import { CartsService } from './carts.service';
 
-import { ProductsResolver } from '../products/products.resolver';
 import { Cart } from '../entities/cart.entity';
 import { Product } from '../entities/product.entity';
 
 @Module({
 	imports: [TypeOrmModule.forFeature([Cart, Product])],
-	providers: [CartsResolver, ProductsResolver, CartsService, CartsRepository],
+	providers: [CartsResolver, CartsService, CartsRepository],
 })
 export class CartsModule {}
--- apps/carts/src/carts/carts.resolver.ts
+++ apps/carts/src/carts/carts.resolver.ts
@@ -1,25 +1,31 @@
-import { Args, Int, Mutation, Query, ResolveReference, Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Parent, Query, ResolveField, ResolveReference, Resolver } from '@nestjs/graphql';
 
 import { CartsService } from './carts.service';
 
 import { Cart } from '../entities/cart.entity';
+import { Product } from '../entities/product.entity';
 
 @Resolver(() => Cart)
 export class CartsResolver {
 	constructor(private readonly cartsService: CartsService) {}
 
 	@Query(() => Cart)
-	async getById(@Args('id') id: number): Promise<Cart> {
+	async getCartById(@Args('id') id: number): Promise<Cart> {
 		return await this.cartsService.getById(id);
 	}
 
 	@Mutation(() => Int)
-	async removeById(@Args('id') id: number): Promise<number> {
+	async removeCartById(@Args('id') id: number): Promise<number> {
 		return await this.cartsService.removeById(id);
 	}
 
 	@ResolveReference()
 	async resolveReference(reference: { __typename: string; id: number }) {
 		return await this.cartsService.getById(reference.id);
+	}
+
+	@ResolveField((of) => [Product])
+	public async products(@Parent() cart: Cart): Promise<Product[]> {
+		return await this.cartsService.getProductsByCartId(cart.id);
 	}
 }
